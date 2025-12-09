7d96aca0eddd99f9d5b15e2440c2f659c4f0756f
srinivasagsr <srinivaslevee@gmail.com>
2025-12-09 07:56:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Dark / Light Mode Toggle
+// DARK MODE TOGGLE
 const toggle = document.getElementById("themeToggle");
 toggle.addEventListener("click", () => {
     document.body.classList.toggle("dark");
     toggle.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
 
-// Expandable Blogs
+// BLOG READ MORE
 document.querySelectorAll(".readmore").forEach(btn => {
     btn.addEventListener("click", () => {
         const parent = btn.parentElement;
